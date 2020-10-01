8356917d24685cdf090defe992e3333e6592d2f6
Leandro Moreno <123857@gmail.com>
2020-10-01 13:33:22 -0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        \SocialiteProviders\Manager\ServiceProvider::class, 
 
         /*
          * Package Service Providers...
